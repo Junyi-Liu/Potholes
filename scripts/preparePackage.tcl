6f6c32f6feaf14a6e0d29acddbff8935ab7ef52c
Junyi-Liu <junyi.liu13@imperial.ac.uk>
2014-08-18 20:16:11 +0100

    change for being compatible with linux OS

--- scripts/preparePackage.tcl
+++ scripts/preparePackage.tcl
@@ -1,10 +1,10 @@
-rename ::source ::Potholes::Source
+rename ::source ::Potholes::Source_1
 
 set ::Potholes::UnsupportedPlatform -1
 
 proc ::source args {
     if {[lsearch $args "-rsrc"] == -1} { 
-	uplevel 1 ::Potholes::Source $args
+	uplevel 1 ::Potholes::Source_1 $args
     } else {
 	throw ::Potholes::UnsupportedPlatform 
     }
