da84c4088d7345daf2e773f18cbe20153d26fb4c
Junyi-Liu <junyi.liu13@imperial.ac.uk>
2014-08-18 21:57:50 +0100

    changes for Vivado HLS

--- scripts/pkgIndex.tcl
+++ scripts/pkgIndex.tcl
@@ -30,9 +30,10 @@ package ifneeded Potholes 1.0 [list apply {dir {
    
     uplevel 1 [list source [file join $dir preparePackage.tcl] ]
 
-    puts "before library setting"
-
     get_platform_library_extension $dir 
+
+    puts "after PROJECT setting"
+
     #uplevel 1 [list source [file join $dir loadPackage.tcl] ] 
     uplevel 1 [list source [file join $dir analysis.tcl] ]
     uplevel 1 [list source [file join $dir scop.tcl] ]
