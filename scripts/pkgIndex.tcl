0b6a166f8540da69eb8326635ee3da5836cfda5d
Junyi-Liu <junyi.liu13@imperial.ac.uk>
2014-08-19 00:11:51 +0100

    changes for Vivado HLS

--- scripts/pkgIndex.tcl
+++ scripts/pkgIndex.tcl
@@ -30,19 +30,7 @@ package ifneeded Potholes 1.0 [list apply {dir {
    
     uplevel 1 [list source [file join $dir preparePackage.tcl] ]
 
-    #get_platform_library_extension $dir 
-
-    switch $::tcl_platform(os) { 
-	Darwin {
-	    load [file join $dir ../lib/libPotholes.dylib] 
-	}
-	Linux { 
-	    load [file join $dir ../lib/libPotholes.so] 
-	}
-	default {
-	    error "Error : Unsupported Platform"
-	}
-    }
+    get_platform_library_extension $dir 
 
     puts "after PROJECT setting"
 
